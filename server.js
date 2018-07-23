c139989e1b09e3e2da94b722119f65920fecd180
wesley9mm <info.developer4@gmail.com>
2018-07-23 09:57:36 -0700

    add

--- server.js
+++ server.js
@@ -12,8 +12,8 @@ app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
 
 // API and HTML routes
-require("./app/routing/apiRoutes.js")(app);
-require("./app/routing/htmlRoutes.js")(app);
+require("./routing/apiRoutes.js")(app);
+require("./routing/htmlRoutes.js")(app);
 
 app.listen(PORT, function() {
 	console.log("App listening on PORT: " + PORT);
